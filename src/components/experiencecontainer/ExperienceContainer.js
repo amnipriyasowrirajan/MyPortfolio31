adc69e557598043518803ecafc758e2ce1025b4b
amnipriyasowrirajan <amnipriya31@gmail.com>
2024-09-23 13:48:02 -0400

    project changes

--- src/components/experiencecontainer/ExperienceContainer.js
+++ src/components/experiencecontainer/ExperienceContainer.js
@@ -8,10 +8,10 @@ const ExperienceContainer = () => {
     <Element className="experiencecontainer" id="experience">
       <h1>Experience</h1>
       <div className="experiencecontainer_info">
-        <Experience number="2" title="Clients" />
+        <Experience number="3" title="Clients" />
         <Experience number="4" title="Individual Projects" />
         <Experience
-          number="6"
+          number="7"
           title="Projects"
           style={{ backgroundColor: "#6528e0" }}
         />
